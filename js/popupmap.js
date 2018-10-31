f48c131726a4df44c4044c7a2a00648c695f113c
Mykhailo <36050805+msvystun@users.noreply.github.com>
2018-10-31 13:43:34 +0200

    123
    1223

---
+++ js/popupmap.js
@@ -0,0 +1,24 @@
+var mapLink = document.querySelectorAll(".contacts-button-map");
+
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+	
+	mapLink.forEach(function (entry) {
+		entry.addEventListener("click", function (event) {
+			event.preventDefault();
+			mapPopup.classList.add("modal-show");
+	});
+
+	mapClose.addEventListener("click", function (evt) {
+		evt.preventDefault();
+		mapPopup.classList.remove("modal-show");
+	});
+
+	window.addEventListener("keydown", function (evt) {
+		if (evt.keyCode === 27) {
+			if (mapPopup.classList.contains("modal-show")) {
+				mapPopup.classList.remove("modal-show");
+			}
+		}
+	});
+});
